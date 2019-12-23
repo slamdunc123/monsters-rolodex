2bb9ed994a1a548d2ecc9f697dd1f014646a6514
Duncan Laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-12-23 14:13:41 +0000

    Updates

---
+++ precache-manifest.916fea02d416e57c131498d1dc959404.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d393bae1b9b1db87d2973224ad73bbc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c611e9948e4c68063cd9",
+    "url": "/monsters-rolodex/static/css/main.6e055f38.chunk.css"
+  },
+  {
+    "revision": "67182f6ca5cf5422df28",
+    "url": "/monsters-rolodex/static/js/2.324d7625.chunk.js"
+  },
+  {
+    "revision": "c611e9948e4c68063cd9",
+    "url": "/monsters-rolodex/static/js/main.ceef2f53.chunk.js"
+  },
+  {
+    "revision": "fe02ad83e192ad6b5373",
+    "url": "/monsters-rolodex/static/js/runtime~main.cca1e3e4.js"
+  }
+]);
